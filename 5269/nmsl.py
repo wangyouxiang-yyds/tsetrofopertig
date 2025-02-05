16b860c91c26d1b78eaf97fa0a10caf41d1a6533
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-05 11:11:36 +0800

    this is force commit without pass through add

--- 5269/nmsl.py
+++ 5269/nmsl.py
@@ -3,3 +3,15 @@ print("nmsl cnmb")
 print("abcdefghijklmnopqrstuvwxyz")
 
 print("MMSMSMSMMSSMSMSMSM")
+
+
+for i in range(100):
+    print("I'm so handsome")
+
+
+i = "wangyuhsiang"
+
+if i != "Wang":
+    print("Not Correct")
+
+
